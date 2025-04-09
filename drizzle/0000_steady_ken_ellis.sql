278fa604cd14f19304303c621e0bffa55a8a6d62
aluno <aluno@INTRAUECA.CEFET.COM>
2025-04-09 17:28:02 -0300

    Atualizei o drizzle

---
+++ drizzle/0000_steady_ken_ellis.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `produto` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`preco` real NOT NULL
+);
--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,7 +1,7 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_steady_domino.sql';
+import m0000 from './0000_steady_ken_ellis.sql';
 
   export default {
     journal,
